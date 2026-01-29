d8c32a478d03b3f0a6f4d4250c45a6a3643410df
Nisar P <nisar@Nisars-MacBook-Air.local>
2026-01-29 23:19:01 +0530

    Debug: Enable error reporting to investigate WSOD

--- core.php
+++ core.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 /**
  * Core utility functions for standalone PHP theme
  * Uses pavilion-gemini API for data access
